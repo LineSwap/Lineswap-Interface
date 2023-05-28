968f7de21a0d1d55abc58104117fc4828159adb2
towerdevswap <akmalhanif672@gmail.com>
2023-05-29 06:47:03 +0700

    fix some image

--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -1,6 +1,6 @@
 import { BinanceWalletConnector } from '@pancakeswap/wagmi/connectors/binanceWallet'
 import { BloctoConnector } from '@pancakeswap/wagmi/connectors/blocto'
-import { cmpMainnet, zeta, linea, bsc, mainnet } from '@pancakeswap/wagmi/chains'
+import { bscTest, zeta, linea, bsc, mainnet } from '@pancakeswap/wagmi/chains'
 import { configureChains, createClient } from 'wagmi'
 import memoize from 'lodash/memoize'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
@@ -10,7 +10,7 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 
-const CHAINS = [linea]
+const CHAINS = [linea, bscTest]
 
 const getNodeRealUrl = (networkName: string) => {
   let host = null
--- packages/uikit/src/components/Spinner/PanIcon.tsx
+++ packages/uikit/src/components/Spinner/PanIcon.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../Svg/types";
 const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
   return (
     <Svg viewBox="0 0 256 256" {...props}>
-      <image width="256" height="256" href="/images/home/lunar-bunny/wallet.png" />
+      <image width="256" height="256" href="/images/home/lunar-bunny/wallet-1.png" />
     </Svg>
   );
 };
